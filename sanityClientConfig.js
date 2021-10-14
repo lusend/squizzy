156a144d1fe8bb9f379cbf34ff769d9edea0286b
P. Christopher Bowers <pcbowers@gmail.com>
2021-10-14 14:46:37 -0400

    updated project id

--- sanityClientConfig.js
+++ sanityClientConfig.js
@@ -1,6 +1,6 @@
 export const sanityClientConfig = {
   // replace this id with the one you find in /studio/sanity.json
-  projectId: 'puj7p168',
+  projectId: 'wh4p4n6y',
   // make sure that the dataset name is the same as the one in /studio/sanity.json
   dataset: 'production',
   useCdn: false
